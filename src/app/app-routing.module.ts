12d768b57146f44dd1117015642bf773ab80b4bd
Claudio Lamur <lamurclaudio@gmail.com>
2024-05-24 15:47:07 -0400

    respaldo app sin chat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes=[
+  {path: '',component: MenuComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+
+}
